6e82424521d727dd1564ee721071bfe5133a49b1
Andrew Carr <53153778+AndrewCarr24@users.noreply.github.com>
2021-02-09 21:59:17 -0500

    added function documentation

--- R/api_credentials_to_token.R
+++ R/api_credentials_to_token.R
@@ -1,4 +1,20 @@
-# Takes API credentials and returns API token 
+#' Takes Twitter API credentials and returns API token.
+#' @param app_name Twitter app name.
+#' @param consumer_key Twitter consumer key.
+#' @param consumer_secret Twitter consumer secret.
+#' @param access_token Twitter access token.
+#' @param access_secret Twitter access secret.
+#' @return Twitter API token.
+#' @examples
+#' ex_app_name = ####
+#' ex_consumer_key = ####
+#' ex_consumer_secret = ####
+#' ex_access_token = ####
+#' ex_access_secret = ####
+#' api_credentials_to_token(app_name = ex_app_name, consumer_key = ex_consumer_key, 
+#'                          consumer_secret = ex_consumer_secret, access_token = ex_access_token, 
+#'                          access_secret = ex_access_secret)
+#' @export
 api_credentials_to_token <- function(app_name, consumer_key, consumer_secret, access_token, access_secret){
   
   # Get app data 

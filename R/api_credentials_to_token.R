d36cfcafac9d7ff77a780a25a24f22a2f6ca8d3e
Andrew Carr <andrew.carr@duke.edu>
2021-02-09 22:30:55 -0500

    added docs

--- R/api_credentials_to_token.R
+++ R/api_credentials_to_token.R
@@ -11,28 +11,28 @@
 #' ex_consumer_secret = ####
 #' ex_access_token = ####
 #' ex_access_secret = ####
-#' api_credentials_to_token(app_name = ex_app_name, consumer_key = ex_consumer_key, 
-#'                          consumer_secret = ex_consumer_secret, access_token = ex_access_token, 
+#' api_credentials_to_token(app_name = ex_app_name, consumer_key = ex_consumer_key,
+#'                          consumer_secret = ex_consumer_secret, access_token = ex_access_token,
 #'                          access_secret = ex_access_secret)
-#' @export
+#'@export
 api_credentials_to_token <- function(app_name, consumer_key, consumer_secret, access_token, access_secret){
-  
-  # Get app data 
+
+  # Get app data
   app <- httr::oauth_app(
     appname = app_name,
     key = consumer_key,
     secret = consumer_secret
   )
-  
-  # Get access token and secret 
+
+  # Get access token and secret
   credentials <- list(oauth_token = access_token,
                       oauth_token_secret = access_secret)
   params <- list(as_header = TRUE)
-  
-  # Put together token 
+
+  # Put together token
   token <- httr::Token1.0$new(app = app, endpoint = httr::oauth_endpoints("twitter"),
                               params = params, credentials = credentials, cache = FALSE)
-  
+
   return(token)
-  
+
 }
--- R/get_user_network.R
+++ R/get_user_network.R
@@ -1,4 +1,4 @@
-# Takes Twitter user information and returns list of data on users in network connections and an edgelist of user's network  
+#' Takes Twitter user information and returns list of data on users in network connections and an edgelist of user's network.
 #' @param id Twitter user id.
 #' @param degree Number of degrees of user's friends/followers to collect.
 #' @param degree Number of "layers" of user's friends/followers to collect.
@@ -12,55 +12,55 @@
 #' ex_id <- 778619636510326784 # Example Twitter user id
 #' ex_degree <- 2 # Collect user's friends and their friends' friends
 #' ex_token <- api_token # Returned from api_credentials_to_token function
-#' get_user_network(id = ex_id, degree = ex_degree, token = ex_token)                           
-#' @export
+#' get_user_network(id = ex_id, degree = ex_degree, token = ex_token)
+#'@export
 get_user_network <- function(id = NULL, degree = 1, token = NULL, track_progress = TRUE, filter_col = NULL, filter_val = NULL, greater = TRUE,
                              base_nodes_edges = NULL, layer_count = 0, collected_ids = NULL, steps = 1){
-  
+
   tryCatch({
-    
-    if(!grepl("^[0-9]+$", id)){stop("User ids can only contain numbers.")} 
-    
+
+    if(!grepl("^[0-9]+$", id)){stop("User ids can only contain numbers.")}
+
     if(is.null(token)){ stop("A Twitter API token is needed.") }
-    
+
     # Ids of friends
     id_object <- id_to_id_object(id, token = token)
-    
+
     # Friend data tibble
-    user_data <- get_user_data(id_object, token = token, filter_col = filter_col, filter_val = filter_val, greater = greater, degree = layer_count+1) 
-    
+    user_data <- get_user_data(id_object, token = token, filter_col = filter_col, filter_val = filter_val, greater = greater, degree = layer_count+1)
+
     # User-friends edgelist (if user has connections)
     user_edgelist <- if(!is.null(user_data)){ tibble(from = id, to = user_data$id) }
-    
+
     # Adding focal user to beginning of user_data (only for first iteration)
     if(layer_count == 0){
       user_data <- bind_rows(get_user_data(id, token = token, degree = 0), user_data)
     }
-    
+
     # Adding to existing network (if one exists)
     if(!is.null(base_nodes_edges) & !is.null(user_data)){
       user_data <- bind_rows(base_nodes_edges[[1]], user_data) %>% filter(!duplicated(screen_name))
       user_edgelist <- bind_rows(base_nodes_edges[[2]], user_edgelist)
     }
-    
-    # Counter goes down when user data / edgelist added 
-    steps <- steps - 1 
-    
+
+    # Counter goes down when user data / edgelist added
+    steps <- steps - 1
+
     if(track_progress & layer_count > 0){
       con_str <- if(steps != 1){"connections"}else{"connection"}
       print(paste0(steps, " remaining ", degree_stringify(layer_count+1), " degree ",  con_str, " to collect."))
     }
-    
-    # layer_count goes up and steps reset when done with steps in a layer 
+
+    # layer_count goes up and steps reset when done with steps in a layer
     if(steps == 0){
-      
-      # Set marker to next degree 
+
+      # Set marker to next degree
       layer_count <- layer_count + 1
-      
+
       # Track Progress (if set to true)
       degree_string <- degree_stringify(layer_count)
       print(paste0("Finished collecting ", degree_string, " degree connections."))
-      
+
       # Only reset steps if there are more layers to collect
       if(layer_count < degree){
         ext_ids <- c(id, collected_ids)
@@ -68,44 +68,44 @@ get_user_network <- function(id = NULL, degree = 1, token = NULL, track_progress
         con_str <- if(steps != 1){"connections"}else{"connection"}
         print(paste0(steps, " remaining ", degree_stringify(layer_count+1), " degree ",  con_str, " to collect."))
       }
-      
+
     }
-    
-    # Return nodes if steps = 0 and layer_count = degree 
+
+    # Return nodes if steps = 0 and layer_count = degree
     if( degree == 1 | (layer_count == degree & steps == 0) ){
-      
+
       return(list(nodes = user_data, edges = user_edgelist))
-      
-      # Reruns function with new values otherwise 
+
+      # Reruns function with new values otherwise
     }else{
-      
+
       collected_ids <- c(collected_ids, id)
-      
+
       # Selecting user id for next iteration
       new_id <- if(!is.null(user_data)){
-        user_data %>% filter(!id %in% collected_ids) %>% pull(id) %>% .[1] 
+        user_data %>% filter(!id %in% collected_ids) %>% pull(id) %>% .[1]
       }else{
-        base_nodes_edges[[1]] %>% filter(!id %in% collected_ids) %>% pull(id) %>% .[1]   
+        base_nodes_edges[[1]] %>% filter(!id %in% collected_ids) %>% pull(id) %>% .[1]
       }
-      
-      # Adding to list of collected data 
+
+      # Adding to list of collected data
       base_nodes_edges <- if(!is.null(user_data)){ list(user_data, user_edgelist) }else{ base_nodes_edges }
-      
-      # Rerun function with values from this iteration as parameters 
-      get_user_network(id = new_id, degree = degree, base_nodes_edges = base_nodes_edges, 
-                       layer_count = layer_count, collected_ids = collected_ids, steps = steps, 
+
+      # Rerun function with values from this iteration as parameters
+      get_user_network(id = new_id, degree = degree, base_nodes_edges = base_nodes_edges,
+                       layer_count = layer_count, collected_ids = collected_ids, steps = steps,
                        token = token, track_progress = track_progress, filter_col = filter_col, filter_val = filter_val, greater = greater)
-      
+
     }
-    
-    
+
+
   }, error = function(e){
-    
+
     print(e)
-    
-    return(list(id = id, degree = degree, base_nodes_edges = base_nodes_edges, layer_count = layer_count, collected_ids = collected_ids, 
+
+    return(list(id = id, degree = degree, base_nodes_edges = base_nodes_edges, layer_count = layer_count, collected_ids = collected_ids,
                 steps = steps))
-    
+
   })
-  
+
 }
